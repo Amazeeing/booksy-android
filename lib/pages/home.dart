d708b1673d777ab8823928063e539f161a019eb9
Amazeeing <fplfrancesco@gmail.com>
2022-12-23 20:31:54 +0100

    Aggiunta operazione di log-out

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:prenotazioni/pages/booking.dart';
 import 'package:prenotazioni/model/utente.dart';
 import 'package:prenotazioni/pages/history.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage(this.user, {Key? key}) : super(key: key);
@@ -14,6 +16,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void _logUserOut() async {
+    await http.post(
+        Uri.parse('http://localhost:8080/progetto_TWeb_war_exploded/autentica?action=scollegaUtente'));
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +33,21 @@ class _HomePageState extends State<HomePage> {
             padding: const EdgeInsets.only(left: 10.0),
             child: Image.asset('assets/logo.png', fit: BoxFit.fitWidth),
           ),
-          leadingWidth: 100.0
+          leadingWidth: 100.0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            iconSize: 20.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {
+              _logUserOut();
+              Navigator.popAndPushNamed(context, '/login');
+            },
+          )
+        ],
       ),
       body: SafeArea(
-          minimum: EdgeInsets.all(50.0),
+          minimum: const EdgeInsets.all(50.0),
           child: Center(
             child: HistoryPage(widget.user)
           )
@@ -37,11 +58,11 @@ class _HomePageState extends State<HomePage> {
           onPressed: () => {
             Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => BookingPage(widget.user))
+                MaterialPageRoute(builder: (context) => BookingPage())
             )
           },
-          label: Text('Prenota'),
-          icon: Icon(Icons.add),
+          label: const Text('Prenota'),
+          icon: const Icon(Icons.add),
         ),
       )
     );
