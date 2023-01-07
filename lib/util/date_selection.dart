a2795c537af61cc432f9a78f5102af9eaf743ebf
Amazeeing <fplfrancesco@gmail.com>
2023-01-07 21:02:09 +0100

    Migliorata gestione stato del widget per la selezione della data

--- lib/util/date_selection.dart
+++ lib/util/date_selection.dart
@@ -3,46 +3,58 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:prenotazioni/util/fields_notifier.dart';
 
-class DateSelection extends ConsumerWidget {
+class DateSelection extends StatefulWidget {
   const DateSelection(this.fieldsProvider, {Key? key}) : super(key: key);
 
-  final StateNotifierProvider<FieldsNotifier, Map<String, String?>> fieldsProvider;
+  final StateNotifierProvider<FieldsNotifier, Map<String, String?>>
+      fieldsProvider;
+
+  @override
+  State<DateSelection> createState() => _DateSelectionState();
+}
+
+class _DateSelectionState extends State<DateSelection> {
+  final _dateInput = TextEditingController();
 
   String _parseDate(DateTime date) {
     return '${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year}';
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text('Seleziona una data'),
         const SizedBox(height: 10.0),
-        TextFormField(
-          validator: (value) {
-            if(value == null || value.isEmpty) {
-              return 'Selezionare una data.';
-            }
+        Consumer(
+          builder: (context, ref, child) {
+            return TextFormField(
+              controller: _dateInput,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Selezionare una data.';
+                }
 
-            return null;
-          },
-          onTap: () async {
-            DateTime today = DateTime.now();
-            DateTime? selected = await showDatePicker(
-              context: context,
-              initialDate: today,
-              firstDate: today,
-              lastDate: today.add(const Duration(days: 7))
+                return null;
+              },
+              onTap: () async {
+                DateTime today = DateTime.now();
+                DateTime? selected = await showDatePicker(
+                    context: context,
+                    initialDate: today,
+                    firstDate: today,
+                    lastDate: today.add(const Duration(days: 7)));
+
+                if (selected != null) {
+                  String parsedDate = _parseDate(selected);
+                  ref.read(widget.fieldsProvider.notifier).setDate(parsedDate);
+                  _dateInput.text = parsedDate;
+                }
+              },
+              decoration: const InputDecoration(border: OutlineInputBorder()),
             );
-
-            if(selected != null) {
-              ref.read(fieldsProvider.notifier).setDate(_parseDate(selected));
-            }
           },
-          decoration: const InputDecoration(
-            border: OutlineInputBorder()
-          ),
         )
       ],
     );
