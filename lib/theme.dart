c83dc10d0f31328c7e3ed7080f99ed877ae07116
Amazeeing <fplfrancesco@gmail.com>
2023-01-07 11:59:53 +0100

    Modificati attributi Card

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,19 +3,14 @@ import 'package:flutter/material.dart';
 final ThemeData booksyTheme = getBooksyTheme();
 
 TextTheme _getBooksyTextTheme(TextTheme base) {
-  return base.copyWith(
-    bodyText2: base.bodyText2!.copyWith(
-        fontWeight: FontWeight.w400,
-        fontSize: 16.0
-    ),
-    button: base.button!.copyWith(
-        fontWeight: FontWeight.w400,
-        fontSize: 16.0,
-        color: Colors.white
-    ),
-  ).apply(
-      fontFamily: 'NotoSans'
-  );
+  return base
+      .copyWith(
+        bodyText2: base.bodyText2!
+            .copyWith(fontWeight: FontWeight.w400, fontSize: 16.0),
+        button: base.button!.copyWith(
+            fontWeight: FontWeight.w400, fontSize: 16.0, color: Colors.white),
+      )
+      .apply(fontFamily: 'NotoSans');
 }
 
 ThemeData getBooksyTheme() {
@@ -25,11 +20,13 @@ ThemeData getBooksyTheme() {
       primaryColor: const Color.fromRGBO(94, 23, 235, 1.0),
       colorScheme: base.colorScheme.copyWith(
           primary: const Color.fromRGBO(94, 23, 235, 1.0),
-          secondary: const Color.fromRGBO(94, 23, 235, 0.7)
-      ),
+          secondary: const Color.fromRGBO(94, 23, 235, 0.7)),
       iconTheme: base.iconTheme.copyWith(
         color: const Color.fromRGBO(94, 23, 235, 0.7),
       ),
-      textTheme: _getBooksyTextTheme(base.textTheme)
+      textTheme: _getBooksyTextTheme(base.textTheme),
+      cardTheme: base.cardTheme.copyWith(
+        elevation: 5.0,
+      )
   );
 }
